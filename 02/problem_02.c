efa641636298e739ba22f66ff9701205cfc23fc6
woodi <woodi.daily@gmail.com>
2021-10-17 16:32:57 +0900

    Solve Problem 02

--- 02/problem_02.c
+++ 02/problem_02.c
@@ -3,21 +3,13 @@
 int i = 0;
 int a[2] = {1, 1};
 
-void p(int x, int y) {
-  x++;
+void p(int *x, int *y) {
+  *x = *x + 1;
   i++;
-  y++;
+  *y = *y + 1;
 }
 
 int main() {
-  p(a[i], a[i]);
+  p(&a[i], &a[i]);
   printf("%d %d\n", a[0], a[1]);
 }
-
-/*
-value: a[0]=1, a[1]=1
-reference: a[0]=3, a[1]=1
-value-result (call): a[0]=2, a[1]=1
-value-result (return): a[0]=1, a[1]=2
-name: a[0]=2, a[1]=2
-*/
--- 02/problem_05.c
+++ 02/problem_05.c
@@ -13,3 +13,11 @@ int main() {
   p(a[i], a[i]);
   printf("%d %d\n", a[0], a[1]);
 }
+
+/*
+value: a[0]=1, a[1]=1
+reference: a[0]=3, a[1]=1
+value-result (call): a[0]=2, a[1]=1
+value-result (return): a[0]=1, a[1]=2
+name: a[0]=2, a[1]=2
+*/
